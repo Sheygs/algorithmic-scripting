a15e313cf674b16eac23285271d4bf94ab6adb36
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-15 01:34:07 +0100

    implement reverseString

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 
 const convertToF = C => 9/5 * C + 32;
 convertToF(10); //50F
+
+//Reverse A String
+
+//1: using built-in method
+const reverseString = str => str.split('').reverse().join('');
+
+reverseString('hello'); // 'olleh'
