29e21738b05c2e2c8a792cbdbb13fdc586555502
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-15 01:43:42 +0100

    reverseString using the decrement for-loop

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ convertToF(10); //50F
 
 //Reverse A String
 
-//1: using built-in method
+//1: Using built-in method
 const reverseString = str => str.split('').reverse().join('');
 
 reverseString('hello'); // 'olleh'
+
+//2: Using a decrement for-loop
+const reverseString = str => {
+  let newString = '', len = str.length;
+  for (let i = str.length - 1; i >= 0; i--){
+     newString+=str[i];
+  }
+  return newString;
+}
+
+reverseString('abaa'); // 'aaba'
+
+
+//2: Using 
