8bf0ceb0d84388f00705581f5f535d416fb96a72
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-20 09:29:48 +0100

    :fire:

--- index.js
+++ index.js
@@ -62,6 +62,10 @@ factorialize(6); //720
 
 
 /*Find The Longest Word In A String*/
+
+/* Beginner */
+
+// Solution 1:
 const findLongestWordLength = str => {
   if (typeof str !== 'string')
      return 'string argument required!';
@@ -78,3 +82,29 @@ const findLongestWordLength = str => {
   return maxLength;   
 }
 findLongestWordLength('Knowledge is evenly distributed'); //distributed
+
+
+
+/* Intermediate */
+
+//solution 2:
+const findLongestWordLength = str => {
+   return str.split(' ') //accepts any string input
+             .reduce((curr,next) => Math.max(curr,next.length), 0);
+}
+
+//Solution 3:
+const findLongestWordLength = str => {
+   const sortedArray = str.match(/[a-z]+/gi)
+                          .sort((x,y) => y.length - x.length);
+     return sortedArray[0].length;                     
+}
+
+//Solution 4:
+const findLongestWordLength = str => {
+   const sortedArray = str.match(/[a-z]+/gi)
+                          .reduce((a,b) => Math.max(a,b.length), 0);
+}
+
+
+/* Advanced */
