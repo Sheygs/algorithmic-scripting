08e167bade9b78bc27d993a1cf1a4dc66407d6c3
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-27 10:27:14 +0100

    :fire:

--- index.js
+++ index.js
@@ -192,3 +192,14 @@ booWho(true); //true
 booWho(false); //true
 booWho([]);  //false
 booWho({"a" : 1}); //false
+
+
+/* Title Case a Sentence */
+//Solution 1:
+const titleCase = sentence => {
+   //convert to array of words
+   const wordArray = sentence.toLowerCase().trim().split(' ');
+   return wordArray.map(el => {
+      return el.charAt(0).toUpperCase() + el.substring(1);
+   }).join(' ');
+}
