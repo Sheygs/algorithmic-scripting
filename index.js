731c592c7479ae2682a93779cca0f54dc51a2424
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-27 09:39:10 +0100

    :fire:

--- index.js
+++ index.js
@@ -165,3 +165,14 @@ const  repeatStringNumTimes = (str,num) => {
    return word;
 }
 
+/* Truncate a String*/
+
+//Solution 1
+const truncateString = (str,num) =>
+      (str.length > num) ? `${str.slice(0,num)}...` : str;
+ 
+truncateString('A-',1)   //A...
+
+//Solution 2
+const truncateString = (str,num) =>
+      (str.length > num) ? str.slice(0,num).padEnd(num + 3,'...') : str;
