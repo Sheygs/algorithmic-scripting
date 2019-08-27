13bf04aade1f6e6fc720cea8b761d30325c0f1f4
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-27 10:30:52 +0100

    :fire:

--- index.js
+++ index.js
@@ -195,11 +195,20 @@ booWho({"a" : 1}); //false
 
 
 /* Title Case a Sentence */
-//Solution 1:
+
+//Solution 1
 const titleCase = sentence => {
    //convert to array of words
    const wordArray = sentence.toLowerCase().trim().split(' ');
    return wordArray.map(el => {
       return el.charAt(0).toUpperCase() + el.substring(1);
+   }).join(' ');
+}
+
+//Solution 2
+const titleCase = sentence => {
+   const wordArray = sentence.toLowerCase().trim().split(' ');
+   return wordArray.map(el => {
+      return el.replace(el.charAt(0), el.charAt(0).toUpperCase());
    }).join(' ');
 }
