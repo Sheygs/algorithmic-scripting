69d9891321c6fac1693b50ef073abf0bdf6d8325
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-11-03 17:00:39 +0100

    :fire:

--- algorithms/index.js
+++ algorithms/index.js
@@ -338,3 +338,7 @@ const sumAll = arr => {
    let sum = (num2 - num1 + 1) * (num1 + num2)/2;
    return sum;
 }
+
+//Seek And Destroy
+const destroyer = (arr,...list) => arr.filter(elt => !list.includes(elt));
+//destroyer([1,2,3,5,1,2,3],2,3) returns [1,5,1]
